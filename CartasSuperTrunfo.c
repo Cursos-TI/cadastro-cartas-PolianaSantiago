7998ea82b69c505efd60c8ddd1451c5fd0c9abfe
Poliana <polianapalmieri@yahoo.com.br>
2025-10-08 10:45:56 +0000

    Programa de Poliana

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,23 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+   char carta'1';
+   char estado='A';
+   char código [20]='A01';
+   char nome, cidade [20]= "São Paulo";
+   int população= 12325000;
+   float área= 1521.11;
+   float PIB= 699.28 Bilhoes, de reais;
+   int número,pontos turísticos: 50;
 
-  // Área para entrada de dados
-
-  // Área para exibição dos dados da cidade
+  scanf("%d", &carta);
+  scanf("%c", &estado);
+  scanf("%d", &código);
+  scanf("%s", &nome da cidade);
+  scanf("%2f", &população);
+  scanf("%d", &área);
+  scanf("%2f", &PIB);
+  scanf("%d", &número de pontos turísticos);
 
 return 0;
 } 
