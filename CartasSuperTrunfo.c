7959fccdbcd3cdfdbba56ec7ffe7ba3903b88409
Poliana <polianapalmieri@yahoo.com.br>
2025-10-21 23:48:03 +0000

    polianasantiago

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int main(){
-   char Carta [2]"1";
+   char Carta[10]= "1";
    char Estado [10]="São Paulo";
-   char Código [4]="A01";
+   char Codigo [4]="A01";
    char Nome_da_cidade [15]= "Ribeirão Preto";
-   int População= 731.639;
-   float Área= 650.916;
-   float PIB= 83.411,"Bilhões";
-   int Número_de_pontos_turísticos:15;
+   int Populaçao= 731639;
+   float Area= 650.916;
+   float PIB= 83.411;
+   int Numero_de_pontos_turisticos=15;
 
-  printf("Carta 1:\n");
-  printf("Estado:São paulo\n");
-  printf("código:A01\n");
-  printf("Nome da Cidade:Ribeirão Preto\n");
-  printf("População:731.639\n");
-  printf("Área:650.916\n")
-  printf("PIB:83,411\n");
-  printf("Número de pontos turísticos:15\n");
+   printf("Carta %s:\n", Carta);
+   printf("Estado: %s\n", Estado);
+   printf("Codigo: %s\n", Codigo);
+   printf("Nome da Cidade: %s\n", Nome_da_cidade);
+   printf("Populaçao: %d\n", Populaçao);
+   printf("Area: %.3f km²\n", Area);
+   printf("PIB: %.3f bilhões\n", PIB);
+   printf("Numero-de-pontos-turisticos: %d\n", Numero_de_pontos_turisticos);
 
 return 0;
 } 
